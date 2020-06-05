cce9abdc606d9d18048adb991f364cf4c56d8b13
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2020-06-05 20:02:51 +0100

    Tried to make things faster by adding things to the controller. Did not work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'nokogiri'
+
 class PagesController < ApplicationController
   def home
   end
@@ -9,5 +12,17 @@ class PagesController < ApplicationController
   end
 
   def retry
+  end
+
+  def faster
+    url = 'https://www.accel.com/'
+    html_file = open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+    @links = html_doc.search('.top-of-mind-preview a')
+
+    url1 = 'https://www.ggvc.com/engage/'
+    html_file1 = open(url1).read
+    html_doc1 = Nokogiri::HTML(html_file1)
+    @links1 = html_doc1.search('.ct-link')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'layout', to: 'pages#layout'
   get 'retry', to: 'pages#retry'
+  get 'faster', to: 'pages#faster'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
