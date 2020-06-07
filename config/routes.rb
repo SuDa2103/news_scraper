b1d503b87fd7164248284fa567f6824fd57a7f69
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2020-06-07 18:13:21 +0100

    new rake page added

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'retry', to: 'pages#retry'
   get 'faster', to: 'pages#faster'
   get 'model', to: 'pages#model'
+  get 'rake', to: 'pages#rake'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ lib/tasks/scraping.rake
@@ -0,0 +1,6 @@
+namespace :scraping do
+  desc 'Fetches new data from websites'
+  task scrape_websites: :environment do
+    # Call your scrapping classes/jobs/whatever code here
+  end
+end
